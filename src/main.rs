7f211fd6fece0740044bb91e6685dd126d8d604d
rocex <rocexwang@gmail.com>
2021-04-26 22:57:55 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::io::prelude::*;
 
 fn main() {
     let a = "hello rust world";
@@ -17,7 +16,29 @@ fn main() {
     for i in 0..10 {
         println!("{2} '{0}' length is {1}", a, ss, i);
     }
-
     let text = fs::read_to_string("D:/DataHub/StudyRust/src/test.d").unwrap();
     println!("{}", text);
+
+    eat_at_restaurant();
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
